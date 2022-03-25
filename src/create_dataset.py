c0aa1933da7c1ba3c791b485fa48678123b1493f
Luke Holman <Holman57@gmail.com>
2022-03-24 23:44:10 -0700

    add configuration file

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -1,30 +1,38 @@
 from argparse import ArgumentParser
 from datasets import load_dataset
 import pandas as pd
+import json
 
 parser = ArgumentParser()
 parser.add_argument('-d', '--data', nargs='+', default=['glue', 'mnli'])
 args = parser.parse_args()
 
 dataset = load_dataset(args.data[0], args.data[1])
+config = {}
 
 df = pd.DataFrame()
 df['premise'] = list(dataset['train']['premise'])
 df['hypothesis'] = list(dataset['train']['hypothesis'])
 df['label'] = list(dataset['train']['label'])
-df.to_csv('../data/train.csv')
+df.to_csv('../data/train.csv', header=False, index=False)
 print('wrote:', len(df), 'rows to train.csv')
+config['train_length'] = len(df)
 
 df = pd.DataFrame()
 df['premise'] = list(dataset['validation_matched']['premise'])
 df['hypothesis'] = list(dataset['validation_matched']['hypothesis'])
 df['label'] = list(dataset['validation_matched']['label'])
 print('wrote:', len(df), 'rows to valid.csv')
-df.to_csv('../data/valid.csv')
+df.to_csv('../data/valid.csv', header=False, index=False)
+config['valid_length'] = len(df)
 
 df = pd.DataFrame()
 df['premise'] = list(dataset['test_matched']['premise'])
 df['hypothesis'] = list(dataset['test_matched']['hypothesis'])
 df['label'] = list(dataset['test_matched']['label'])
 print('wrote:', len(df), 'rows to test.csv')
-df.to_csv('../data/test.csv')
+df.to_csv('../data/test.csv', header=False, index=False)
+config['test_length'] = len(df)
+
+with open('../data/params.json', 'w') as f:
+    json.dump(config, f)
