43da5684e9c75309a1eb4d8a27b5da577aff72eb
Luke Holman <Holman57@gmail.com>
2022-03-24 23:10:45 -0700

    save datasets locally using load_dataset from transformers

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -1,11 +1,30 @@
 from argparse import ArgumentParser
 from datasets import load_dataset
+import pandas as pd
 
 parser = ArgumentParser()
-parser.add_argument('-m', '--model', default='bert-base-cased')
-parser.add_argument('-b', '--batch_size', default=32)
-parser.add_argument('-e', '--epochs', default=10)
+parser.add_argument('-d', '--data', nargs='+', default=['glue', 'mnli'])
+args = parser.parse_args()
 
-mnli_dataset = load_dataset('glue', 'mnli')
+dataset = load_dataset(args.data[0], args.data[1])
 
+df = pd.DataFrame()
+df['premise'] = list(dataset['train']['premise'])
+df['hypothesis'] = list(dataset['train']['hypothesis'])
+df['label'] = list(dataset['train']['label'])
+df.to_csv('../data/train.csv')
+print('wrote:', len(df), 'rows to train.csv')
 
+df = pd.DataFrame()
+df['premise'] = list(dataset['validation_matched']['premise'])
+df['hypothesis'] = list(dataset['validation_matched']['hypothesis'])
+df['label'] = list(dataset['validation_matched']['label'])
+print('wrote:', len(df), 'rows to valid.csv')
+df.to_csv('../data/valid.csv')
+
+df = pd.DataFrame()
+df['premise'] = list(dataset['test_matched']['premise'])
+df['hypothesis'] = list(dataset['test_matched']['hypothesis'])
+df['label'] = list(dataset['test_matched']['label'])
+print('wrote:', len(df), 'rows to test.csv')
+df.to_csv('../data/test.csv')
